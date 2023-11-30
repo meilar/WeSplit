e5e0c99db2d91b2e113a25afb28651613e1e08cc
Matt Eilar <meilar@gmail.com>
2023-11-30 11:42:17 -0800

    Initial Commit

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Matt Eilar on 11/30/23.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
